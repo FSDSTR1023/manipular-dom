d77e8fd2bb90ca5fc40bffaf9a10f89eac880c59
crispi <cristina.picatoste@gmail.com>
2023-10-22 00:10:18 +0200

    feat: excercice

---
+++ js/script01.js
@@ -0,0 +1,27 @@
+let element;
+
+element = document;
+element = document.all;
+element = document.head;
+element = document.body;
+element = document.domain;
+
+element = document.forms;
+element = document.forms[1];
+element = document.forms[0].id;
+element = document.forms[0].method;
+element = document.forms[0].classList;
+element = document.forms[0].classList[1];
+
+element = document.links;
+element = document.links[7];
+element = document.links[7].classList;
+element = document.links[7].className;
+
+element = document.images;
+
+element = document.scripts;
+
+
+
+//console.log(element);
