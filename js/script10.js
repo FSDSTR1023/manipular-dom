a3b6b1e6bf48999bf1170a11c7f33e08b2405e49
m-marinucci <mmarinucci@numinate.com>
2023-10-27 00:38:48 +0200

    Intento de Massi, falta algo que no pillo

--- js/script10.js
+++ js/script10.js
@@ -1,31 +1,42 @@
+// Creación del contenedor principal de la sección:
+const sectionWrapper = document.createElement('div');
+sectionWrapper.classList.add('row', 'featurette');
 
-<div class="row featurette">
-            <div class="col-md-7">
-                <h2 class="fw-normal lh-1">Lorem ipsum dolor sit amet. <span class="text-muted">Lorem, ipsum
-                        dolor.</span></h2>
-                <p class="lead">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex reiciendis optio maiores
-                    deserunt minima perspiciatis dolore dolor quo fuga, voluptatum tenetur fugiat cum culpa qui est hic
-                    adipisci vero obcaecati autem architecto aliquam asperiores. Atque adipisci provident nostrum
-                    voluptas expedita suscipit optio, impedit laborum enim sed! Expedita temporibus sed similique?</p>
-            </div>
-            <div class="col-md-5">
-                <img class="img-thumbnail" src="img/countryside.jpg" alt="">
-            </div>
-        </div>
+// Contenedor y contenido para la parte de texto:
+const textWrapper = document.createElement('div');
+textWrapper.classList.add('col-md-7');
 
-        
-    const wrapper = document.vcreateElement('div');
-    wrapper.classList = 'row featurette'; 
-    
-    const wrapperText = document.createElement('div');
-    wrapperText.classList = 'col-mdm7';
+const title = document.createElement('h2');
+title.classList.add('fw-normal', 'lh-1');
+title.textContent = " Pedazo Templo ";
 
-    const wrapperText = document.createElement('p');
-    wrapperText.classList = 'lead';
-    wrapperTextP.textContent = 'Soy el super texto nuevo que has creado pedazo de maquina'
-    
-    wrapper.appenChild(wrapperText);
-    wrapperText.appenChild(wrapperTextP);
-    
-    const insertToDocument = document.querySelector('#customDOM');
-    insertToDocument.appenChild(wrapper);
+const titleSpan = document.createElement('span');
+titleSpan.classList.add('text-muted');
+titleSpan.textContent = " Hay que ir a verlo ";
+title.appendChild(titleSpan);
+
+const text = document.createElement('p');
+text.classList.add('lead');
+text.textContent = "Este es el Templo de Angkor Wat.";
+
+textWrapper.appendChild(title);
+textWrapper.appendChild(text);
+
+// Contenedor y contenido para la imagen:
+const imgWrapper = document.createElement('div');
+imgWrapper.classList.add('col-md-5');
+
+const image = document.createElement('img');
+image.classList.add('img-thumbnail');
+image.src = "img/temple.jpg";
+image.alt = "Templo de Angkor Wat";
+
+imgWrapper.appendChild(image);
+
+// Añadir los contenedores de texto e imagen al contenedor principal:
+sectionWrapper.appendChild(textWrapper);
+sectionWrapper.appendChild(imgWrapper);
+
+// Inyectar el contenedor principal en el div con id "customDOM":
+const customDOMDiv = document.querySelector('#customDOM');
+customDOMDiv.appendChild(sectionWrapper);
